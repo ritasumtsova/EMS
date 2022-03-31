51b40352a24ef81bbb555911dd4ab58179e45fe2
ritasumtsova <48488494+ritasumtsova@users.noreply.github.com>
2022-03-31 10:15:38 +0300

    fixed an issue in the LogoutButton component

--- src/components/LogoutButton/LogoutButton.jsx
+++ src/components/LogoutButton/LogoutButton.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Redirect } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { Button } from 'reactstrap';
 import './LogoutButton.scss';
 
@@ -8,34 +8,27 @@ class LogoutButton extends Component {
     super(props);
 
     this.state = {
-      isAuth: true,
+
     };
 
     this.logoutHandler = this.logoutHandler.bind(this);
   }
 
+  // eslint-disable-next-line class-methods-use-this
   logoutHandler() {
     localStorage.removeItem(process.env.REACT_APP_TOKEN);
-
-    this.setState({
-      isAuth: false,
-    });
   }
 
   render() {
-    const { isAuth } = this.state;
-
-    if (!isAuth) { //  DOES NOT WORK IN THE RIGHT WAY
-      <Redirect to="/" />;
-    }
-
     return (
-      <Button
-        color="primary"
-        onClick={this.logoutHandler}
-      >
-        Logout
-      </Button>
+      <Link to="/">
+        <Button
+          color="primary"
+          onClick={this.logoutHandler}
+        >
+          Logout
+        </Button>
+      </Link>
     );
   }
 }
